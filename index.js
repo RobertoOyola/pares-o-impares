f66e2b07b0c7edf88a59c947dd7e2cd671ff4a3c
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-11-02 11:07:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+var numeroacomprobar = 5;
+
+if (numeroacomprobar == 0){
+    console.log(`su numero es un ${numeroacomprobar}`)
+}else{
+    if(numeroacomprobar % 2 == 0){
+        console.log(`el numero ${numeroacomprobar} es un par`)
+    }else{
+        console.log(`el numero ${numeroacomprobar} es un impar`)
+    }
+}
